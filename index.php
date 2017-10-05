70315f17f3979da5af224542abc3377ae42181e9
Andrii Popov <andrew.p@codesushi.co>
2017-10-05 16:57:55 +0000

    make template

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
+<?php
+
+$a = rand(-5, 5);
+$b = rand(0, 1);
+
+if ($b) {
+    $c = $a / $b;
+}
+
+// $output = isset($c) ? 'a/b = ' . $c : 'Division by zero';
+
+if (isset($c)) {
+    $output = "a/b = {$c}";
+} else {
+    $output = 'Division by zero';
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,12 +27,13 @@
 </head>
 <body>
     
-    <?php
-        
-        
-        
-    ?>
+    <h1>Calculator</h1>
     
+    a =  <?=$a ?>
+    <br>
+    b =  <?=$b ?>
+    <hr>
+    Result: <?=$output ?>
     
 </body>
 </html>
